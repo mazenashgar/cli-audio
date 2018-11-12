66ab1d6cfb156e36bf09e50f293260ffb6062f10
Isfar Baset <43085165+IsfarBaset19@users.noreply.github.com>
2018-11-12 16:12:26 -0500

    Add files via upload
    Player.py has been commented

---
+++ player/Player.py
@@ -0,0 +1,65 @@
+"""PyAudio Example: Play a wave file (callback version)."""
+
+import pyaudio
+import wave
+import time
+
+class Player:
+
+    playingNow = False #variable set to false
+    trackPlaying = ""
+    def __init__(self): #constructor method for the class
+        self.currentSong = "Nothing playing."
+        self.paused = True
+        self.position = 0
+
+    def getCurrentSong(self):
+        if self.playingNow == True: #if a song is playing
+            if self.stream.is_stopped() or self.paused == True: #if song is stopped or paused
+                self.currentSong = "Nothing playing." #display string
+                    return self.currentSong #string message returned
+
+    def pause(self):
+        if self.paused == False: #if a song is not paused
+            self.paused = True #set paused to true
+            self.stream.stop_stream() #pause the song
+            #self.currentSong = "Nothing playing."
+        else: #if a song is paused
+            self.paused = False #set paused to false
+            self.stream.start_stream() #start playing song
+            self.currentSong = self.trackPlaying #display name of current song playing
+
+    def play(self, track):
+        self.paused = False #set paused to false
+        self.currentSong = track #display name of current song playing
+        self.trackPlaying = track #display name of current song playing
+        track = '/Users/mazenashgar/Desktop/GVSU/CIS 343/PythonProject/cli-audio/media/' + track + '.wav' #get the name of track from the provided directory
+        self.wf = wave.open(track, 'rb') #acquire track
+
+        # instantiate PyAudio
+        self.p = pyaudio.PyAudio()
+
+        # open self.stream using callback (
+        self.stream = self.p.open(format=self.p.get_format_from_width(self.wf.getsampwidth()),
+                channels=self.wf.getnchannels(),
+                rate=self.wf.getframerate(),
+                output=True,
+                stream_callback=self.callback)
+
+        # start the self.stream
+        self.stream.start_stream()
+        self.playingNow = True
+    
+    #method defined for stop playing song
+    def stop(self):
+        self.stream.stop_stream()
+        self.stream.close()
+        self.wf.close()
+
+        self.p.terminate() 
+    
+    #method defined for replaying song
+    def callback(self, in_data, frame_count, time_info, status):
+        data = self.wf.readframes(frame_count)
+        return (data, pyaudio.paContinue)
+
